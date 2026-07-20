572309dd75ee000e7f8d1815fcb99f0bc3db4608
Hu man <romaincontant.80@gmail.com>
2026-07-20 13:25:23 +0100

    Create 01_CreateTables.sql

---
+++ DATABASE/01_CreateTables.sql
@@ -0,0 +1,7 @@
+-- Création de la table des Joueurs
+CREATE TABLE IF NOT EXISTS Users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(50) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
